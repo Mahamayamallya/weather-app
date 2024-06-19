7e84431e0ff5bce90edddb0ea9cbee2fa444118f
Mahamaya Mallya <mahamayamallya4@gmail.com>
2024-06-19 09:27:02 +0530

    update css,vertical  scrolling

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ function getWeather() {
   fetch(forecastUrl)
     .then((response) => response.json())
     .then((data) => {
-      displayHourlyForecast(data.list);
       displayDailyForecast(data.list);
     })
     .catch((error) => {
@@ -52,12 +51,10 @@ function displayWeather(data) {
   const tempDivInfo = document.getElementById("temp-div");
   const weatherInfoDiv = document.getElementById("weather-info");
   const weatherIcon = document.getElementById("weather-icon");
-  const hourlyForecastDiv = document.getElementById("hourly-forecast");
   const dailyForecastDiv = document.getElementById("daily-forecast");
 
   // Clear previous content
   weatherInfoDiv.innerHTML = "";
-  hourlyForecastDiv.innerHTML = "";
   dailyForecastDiv.innerHTML = "";
   tempDivInfo.innerHTML = "";
 
@@ -71,7 +68,7 @@ function displayWeather(data) {
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
     const temperatureHTML = `<p>${temperature}°C</p>`;
-    const weatherHtml = `<p>${cityName}</p> <p>${description}</p>`;
+    const weatherHtml = `<span>${cityName}</span><span> | <span>${description}</span>`;
 
     tempDivInfo.innerHTML = temperatureHTML;
     weatherInfoDiv.innerHTML = weatherHtml;
@@ -80,31 +77,6 @@ function displayWeather(data) {
 
     showImage();
   }
-}
-
-function displayHourlyForecast(hourlyData) {
-  const hourlyForecastDiv = document.getElementById("hourly-forecast");
-
-  const next24Hours = hourlyData.slice(0, 8); // 24 hours (3-hour intervals)
-
-  next24Hours.forEach((item) => {
-    //console.log("hello");
-    const dateTime = new Date(item.dt * 1000); // Convert to milliseconds
-    const hour = dateTime.getHours();
-    const temperature = Math.round(item.main.temp - 273.15); // Convert to Celsius
-    const iconCode = item.weather[0].icon;
-    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-
-    const hourlyItemHtml = `
-              <div class="hourly-item">
-                  <span>${hour}:00</span>
-                  <img src="${iconUrl}" alt="Hourly Weather Icon">
-                  <span>${temperature}°C</span>
-              </div>
-          `;
-
-    hourlyForecastDiv.innerHTML += hourlyItemHtml;
-  });
 }
 
 function displayDailyForecast(forecastData) {
@@ -143,3 +115,7 @@ function showImage() {
   const weatherIcon = document.getElementById("weather-icon");
   weatherIcon.style.display = "block"; // Make the image visible once it's loaded
 }
+// function hideSearchButton(){
+//   const searchButton = document.getElementById("search-button");
+//   searchButton.style.display=none;
+// }
